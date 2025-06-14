c3a210b9a0e73692962538ba6e5d8ae3a1fc9ea9
Jerry Okafor <jerry.okafor@bethinklabs.com>
2025-06-14 02:40:43 -0600

    chore: update dependencies

--- core/config/build.gradle.kts
+++ core/config/build.gradle.kts
@@ -25,8 +25,7 @@ buildkonfig {
 
     // default config is required
     defaultConfigs {
-        
-        //LinkedIn
+        // LinkedIn
         buildConfigField(STRING, "linkedInClientId", secretProps["LINKEDIN_CLIENT_ID"] as String)
         buildConfigField(STRING, "redirectUrl", secretProps["REDIRECT_URL"] as String)
         buildConfigField(
@@ -34,9 +33,8 @@ buildkonfig {
             "linkedInClientSecret",
             secretProps["LINKEDIN_CLIENT_SECRET"] as String,
         )
-        
         // X or Twitter
-        buildConfigField(STRING,"xClientId",secretProps["X_OAUTH_CLIENT_ID"] as String)
+        buildConfigField(STRING, "xClientId", secretProps["X_OAUTH_CLIENT_ID"] as String)
     }
 
     targetConfigs {
